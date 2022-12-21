deeb7fb0fb6f675517c7835277653ab59834d652
Milton <miltonsoftwareengineer@gmail.com>
2022-12-21 14:42:07 +0300

    updated work

---
+++ index.js
@@ -0,0 +1,44 @@
+// Ternary Operator
+// const age = 26;
+// const isAdult = age >= 18 ? true : false;
+
+// isAdult;
+
+//Switch
+
+//Switch,case,break,default
+const order = 'cheeseburger';
+
+let ingredients;
+
+switch (order) {
+    case 'cheeseburger':
+        ingredients = 'bun, burger, cheese, lettuce, tomato, onion';
+        break;
+    case 'hamburger':
+        ingredients = 'bun, burger, lettuce, tomato, onion';
+        break;
+    case 'malted':
+        ingredients = 'milk, ice cream, malted milk powder';
+        break;
+    default:
+        console.log("Sorry, that's not on the menu right now.");
+        break;
+}
+
+
+const age = 25;
+
+let isAdult, canWork, canEnlist, canDrink;
+
+switch (true) {
+    case age >= 21:
+        canDrink = true;
+    case age >= 18:
+        isAdult = true;
+        canEnlist = true;
+    case age >= 16:
+        canWork = true;
+}
+
+
